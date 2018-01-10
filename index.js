20ec504c4104aa4050708b981e970e18d4ea84fa
zgreathouse <greathouse.zachary@gmail.com>
2018-01-10 15:20:28 -0800

    enable cookies in app, install cookie-session, wire it up in index.js file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/dev');
 
 //class Models
@@ -14,6 +16,15 @@ mongoose.connect(keys.mongoURI, {
 
 //initialize instance of express
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 2592000000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 //routes
 require('./routes/authRoutes')(app);
