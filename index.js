66aabc2a17b8c075d952cf1aca2a9b2b84704069
zgreathouse <greathouse.zachary@gmail.com>
2018-01-10 14:55:31 -0800

    create User collection (model class), build out passport.js file

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 
+//class Models
+require('./models/User');
+
 //service
 require('./services/passport');
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => { done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
