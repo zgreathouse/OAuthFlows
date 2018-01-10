e30ee7186b1dfcfa2dfd216bfa32c450a8db52ac
zgreathouse <greathouse.zachary@gmail.com>
2018-01-10 14:46:53 -0800

    install mongoose and hook it up to index.js file

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
+//service
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, {
+  useMongoClient: true
+});
+
+//initialize instance of express
 const app = express();
 
+//routes
 require('./routes/authRoutes')(app);
 
-const PORT = precess.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
